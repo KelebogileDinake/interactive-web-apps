5061336d7d5ea4e983aaaa750126a50348756a63
KelebogileDinake <kelebogiledinake97@gmail.com>
2023-03-28 13:52:52 +0200

    fixed challenge 2 and 3, add an html file and ReadMe

---
+++ IWA2/README.md
@@ -0,0 +1,5 @@
+# interactive-web-apps
+Free Code Camp link: Kelebogile_Dinake
+
+for challenge 1: the orignal code didn't work because
+The comments were wrong and they affected the java code.
--- IWA2/challenge3.js
+++ IWA2/challenge3.js
@@ -5,8 +5,10 @@ It is important to show the following message in order to:
 - to impress investors
 */
 
-console.warn(securityScanStrating)
+const warn="securityScanStrating"
 
 /* It is important to let user know when they can close the page  */
 
-console.info(pleaseWaitForScanToCompleteBeforeClosingTheBrowser)
+const info="pleaseWaitForScanToCompleteBeforeClosingTheBrowser"
+
+console.log(warn + info)
