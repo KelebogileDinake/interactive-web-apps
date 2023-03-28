1c073a5892a01ec96e555751a1ee7d6d1283337f
KelebogileDinake <kelebogiledinake97@gmail.com>
2023-03-28 11:25:53 +0200

    created a js files and completed the challenge 1to3

---
+++ IWA2/challenge3.js
@@ -0,0 +1,12 @@
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn(securityScanStrating)
+
+/* It is important to let user know when they can close the page  */
+
+console.info(pleaseWaitForScanToCompleteBeforeClosingTheBrowser)
