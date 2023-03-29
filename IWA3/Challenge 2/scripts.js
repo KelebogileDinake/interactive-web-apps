947d8d9db62e1e0192d8be389165dc19b7979b75
KelebogileDinake <kelebogiledinake97@gmail.com>
2023-03-29 16:46:28 +0200

    update

--- IWA3/Challenge 2/scripts.js
+++ IWA3/Challenge 2/scripts.js
@@ -3,4 +3,4 @@ import { roleA } from "./alex";
 import { roleB } from "./johannes";
 import { roleC } from "./nwabisa";
 
-console.log('Roles:', roleA, roleB, roleC,);
+console.log( 'Roles' roleA, roleB, roleC,);
