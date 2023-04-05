0d10f5191e576c2487643e69194d1d15112e5e4f
KelebogileDinake <kelebogiledinake97@gmail.com>
2023-04-05 14:36:24 +0200

    created file challenge 1,2 and solved both of them

---
+++ IWA 7/challenge1.js
@@ -0,0 +1,9 @@
+//From LMS
+//const value = "3" 
+//console.log(value + 4 + value)
+//output is 343 because the value is a string of 3
+
+//My Solution
+const value = 3 // remove 3 from the string
+console.log(value + 4 + value)
+//output is 10 becase the value is no longer a string but a number 3
---
+++ IWA 7/challenge2.js
@@ -0,0 +1,15 @@
+//From LMS
+//const nickname= "Timmy";
+//const firstname = "Timothy";
+
+//console.log("Good Morning, ${nickname} || {firstname}!")
+
+//My Solution
+const nickname = "Timmy";
+const firstname= "Timothy"
+
+console.log(`Good Morning!, ${nickname} || ${firstname}`)
+
+//i fixed the code by add a $ next to a firstname and deleted the ||
+//removed the string on console.log 
+//to select either a Timmy or Timothy i have to remove a name either of the 2 
