01eb1d0b94e26ca8886295f1353de788afc85d34
KelebogileDinake <kelebogiledinake97@gmail.com>
2023-04-05 16:44:49 +0200

    fixed challenge 1 and challenge 3

--- IWA 7/challenge1.js
+++ IWA 7/challenge1.js
@@ -4,6 +4,11 @@
 //output is 343 because the value is a string of 3
 
 //My Solution
-const value = 3 // remove 3 from the string
-console.log(value + 4 + value)
+//const value = 3 // remove 3 from the string
+//console.log(value + 4 + value)
 //output is 10 becase the value is no longer a string but a number 3
+
+//Another solution
+const value = parseInt("3") // INtroduce parseInt
+console.log(value + 4 + value)
+//output will be 10
--- IWA 7/challenge3.js
+++ IWA 7/challenge3.js
@@ -10,11 +10,14 @@ const sarahBalance = '-4582.21000111'
 const divider = '----------------------------------'
 
 // Only change below this line
+let sarahNewBalance= sarahBalance*-1
+let leoNewBalance= leoBalance*-1
 
-const owed = parseInt('R' + leoBalance + sarahBalance)
-const leo = "{leoName} + {leoSurname} + \"Owed\" + \"R\" + {sarahBalance}"
-const sarah = "{leoName} + {surname} + \"Owed\" + \"R\" + {sarahBalance}"
-const total = "Total amount owed: "
-const result = leo + sarah + divider + divider + total + owed + divider
+
+const owed = parseInt( leoNewBalance + sarahNewBalance)
+const leo = (`${leoName} ${(leoSurname).trim()} Owed R: ${leoNewBalance}`)
+const sarah = (`${(sarahName).trim()} ${sarahSurname} Owed R:${sarahNewBalance.toFixed(2)}`)
+const total = "Total amount owed: R "
+const result = (`${leo} \n${sarah} \n \n${divider} \n${total} ${owed} \n${divider}`)
 
 console.log(result)
