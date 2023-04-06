b7f463b01c8c07cccd481c1e663057292b6e4c3b
KelebogileDinake <kelebogiledinake97@gmail.com>
2023-04-06 13:18:59 +0200

    update

--- IWA6/Challenge 1/scripts.js
+++ IWA6/Challenge 1/scripts.js
@@ -11,19 +11,28 @@ console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid)*/
 
 //My solution
-const primaryPhone = 'O748105141'
-const secondaryPhone = '0219131568'
+//const primaryPhone = 'O748105141'
+//const secondaryPhone = '0219131568'
 
 //Only change below this line
 
 //const primaryValid = typeof number == primaryPhone
 //const secondaryValid = typeof secondaryPhone == 'string' // only changed and use the type of secondaryPhone as it is a globe const and decleared it a string because it is a string
 
-const primaryValid = !Number.isNaN(parseInt(primaryPhone))
-const secondaryValid = !Number.isNaN(parseInt(secondaryPhone))
-
-console.log('Primary phone is valid numerical string:' , primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid)
+//console.log('Primary phone is valid numerical string:' , primaryValid)
+//console.log('Secondary phone is valid numerical string:', secondaryValid)
 
 //Olebogeng solution
 //const secondaryValid typeof number parseInt(primaryPhone) === primaryPhone
+
+//Coach's Solution
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = !Number.isNaN(parseInt(primaryPhone))
+const secondaryValid = !Number.isNaN(parseInt(secondaryPhone))
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid)*
