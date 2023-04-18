ab5c9cb9a44b441eb64a47aee2bfd029823e9f79
KelebogileDinake <kelebogiledinake97@gmail.com>
2023-04-18 11:40:37 +0200

    UPDATE

---
+++ IWA14/challenge2.js
@@ -0,0 +1,78 @@
+/* //LMS
+function add a, b => { a + b }
+
+function multiply a, b => { a - b }
+
+function internal() {
+	const added = this.add(this.a, this.b)
+	this.multiply(this.a, this.b)
+	return this
+}
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+example1.calculate()
+example2.calculate()*/
+
+//solution
+function add (a, b) {
+    return a + b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function internal() {
+    const added = this.add(this.internal.a, this.internal.b);
+    const multipied = this.multiply(this.internal.a, this.internal.b);
+};
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+console.log(example1.calculate()) 
+console.log(example2.calculate())
---
+++ IWA14/scripts.js
@@ -0,0 +1,37 @@
+/* //LMS
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = () => {
+  console(parameter)
+  console(parameter)
+}
+
+function hobby () {
+  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+}
+
+hobby()*/
+
+//Solution
+const firstName = 'John';
+const age= 35;
+const hobby = 'Coding';
+
+const logTwice= (parameter) => {
+    console.log(parameter)
+    console.log(parameter)
+}
+
+function logHobby() {
+    logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+logHobby()
+
+//the code didn't work because it missed const at the top, it had syntax errors
+//the arrow function on line 22 logTwice missed prameter that can be used
+//renamed 'hobby' to logHobby to avoid conflict with a hobby variable
+//changed the name to firstName 
+//called the logHobby function to execute the code
