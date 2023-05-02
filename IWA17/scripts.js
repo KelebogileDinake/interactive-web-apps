8335780a9b45c78872cbe730b7139af8951d0d09
KelebogileDinake <kelebogiledinake97@gmail.com>
2023-05-02 12:29:38 +0200

    update

--- IWA17/scripts.js
+++ IWA17/scripts.js
@@ -1,5 +1,5 @@
-// scripts.js
-const MONTHS = [
+//My Solution
+  const MONTHS = [
     'January',
     'February',
     'March',
@@ -12,71 +12,199 @@ const MONTHS = [
     'October',
     'November',
     'December',
-]
-const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
-// Only edit below
-const createArray = (length, defaultValue) => {
-  return Array.from({ length }, () => defaultValue);
-};
-const createData = () => {
-  const current = new Date();
-  current.setDate(1);
-  const startDay = current.getDay();
-  const daysInMonth = getDaysInMonth(current);
-  const weeks = createArray(6, { week: 0, days: createArray(7, null) });
-  let value = null;
-  for (let weekIndex in weeks) {
-    value = {
-      week: parseInt(weekIndex) + 1,
-      days: [],
-    };
-    for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
-      const day = weekIndex * 7 + dayIndex - startDay + 1;
-      const isValid = day > 0 && day <= daysInMonth;
-      value.days.push({
-        dayOfWeek: dayIndex + 1,
-        value: isValid ? day : null,
-      });
+  ];
+  
+  
+  const getDaysInMonth = (year, month) => new Date(year, month + 1, 0).getDate();
+  //this function getDaysInMonth takes a year and a month as arguments, and 
+  //returns the number of days in the specified month of the specified year
+  
+  // Only edit below
+  
+  //Note, for me to be able to fix this, I had to change most of the code if not all of it.
+  const createData = () => { //added const to createData and assigned it to the following variables
+    // Below are the variables that were assigned by the createData constant
+    //const current = new Date();
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth();
+    const daysInMonth = getDaysInMonth(year, month);
+  //Intialize an empty array to hold the weeks of the calendar
+    const weeks = [];
+    let week = [];
+  
+    // Find the first day of the month and to find correct starting point for the calender 
+    const firstDay = new Date(year, month, 1).getDay();
+  
+    // Offset for starting week from Saturday
+    let offset = firstDay < 6 ? firstDay + 1 : 6;
+  
+    for (let i = 0; i < offset; i++) {
+      week.push(null); // filled in empty cells for days before the first day of the month
     }
-    weeks[weekIndex] = value;
-  }
-  return weeks;
-};
-const addCell = (classString, value, isToday) => {
-    if (isToday) {
-      classString += ' table_cell_today'; // add the "table_cell_today" class for the current date
-      value = `${value}`; //To add the color to the text
+  
+    for (let day = 1; day <= daysInMonth; day++) {
+      week.push(day); // Used a for loop to add each day of the month to the week array
+  
+      if (week.length === 7) {
+        // if week array has 7 days, push it to weeks array and start a new week
+        weeks.push(week);
+        week = [];
+      }
     }
-    return /* html */ `
-      <td class="${classString}">
-        ${value}
-      </td>
-    `;
+  
+    // Push the last week to weeks array
+    if (week.length > 0) {
+      weeks.push(week);
+    }
+  
+    return weeks;
+    
   };
-const createHtml = (data) => {
-  let result = '';
-  for (let week of data) {
-    let inner = '';
-    inner += addCell('table_cell table_cell_sidebar', `Week ${week.week}`);
-    for (let day of week.days) {
-      let classString = 'table__cell';
-      const today = new Date();
-      today.setHours(0, 0, 0, 0); // set the time to 00:00:00:00
-      const isToday =
-        day.value &&
-        new Date(current.getFullYear(), current.getMonth(), day.value).getTime() === today.getTime();
-      const isWeekend = day.dayOfWeek === 1 || day.dayOfWeek === 7;
-      const isAlternate = week.week % 2 === 0;
-      if (isWeekend) classString += ' table__cell_weekend';
-      if (isAlternate) classString += ' table__cell_alternate';
-      inner += addCell(classString, day.value || '', isToday);
+  
+  const createCell = (day, isToday) => {
+    const cell = document.createElement('td');
+    cell.classList.add('table__cell');
+  
+    if (day !== null) { //This is to check if day is not null
+      cell.innerText = day;
+      if (isToday) {// Check if isToday is true
+        cell.classList.add('table__cell_today');
+      }
+    } 
+    return cell;
+  };
+  //the createHtml function creates a tablecell ('td') with the given day and a class 
+  const createHtml = () => {
+    const content = document.querySelector('[data-content]');
+    const weeks = createData();
+  
+    for (let i = 0; i < weeks.length; i++) {
+      const week = weeks[i];
+      const row = document.createElement('tr');
+  
+      // Create and append the week number cell
+      const weekNumber = document.createElement('td');
+      weekNumber.classList.add('table__cell_week');
+      weekNumber.innerText = `Week ${i + 1}`;
+      row.appendChild(weekNumber);
+  
+      for (let j = 0; j < week.length; j++) {
+        const day = week[j];
+        const isToday = day === new Date().getDate() && new Date().getMonth() === new Date().getMonth();
+   
+        row.appendChild(createCell(day, isToday));
+      }
+  
+      content.appendChild(row);
     }
-    result += `<tr>${inner}</tr>`;
   }
-  return result;
-};
-// Only edit above
+  // Only edit above
+  
   const current = new Date();
   document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`;
-  const data = createData();
-  document.querySelector('[data-content]').innerHTML = createHtml(data);
+  
+  createHtml();
+  /*const data = createData()
+document.querySelector('[data-content]').innerHTML = createHtml(data)*/
+
+/*//From LMS
+// scripts.js
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+
+// Only edit below 
+
+const createArray = (length) => {
+  const result = []
+
+  for (0, i, length) {
+      result
+  }
+}
+
+const createData = () => {
+  const current = new Date
+  current.setDate(1)
+
+  startDay = current.day
+  daysInMonth = getDaysInMonth(current)
+
+  weeks = createArray(5)
+  days = createArray(7)
+  value = null
+
+  for (weekIndex in weeks) {
+      value = [{
+          week: weekIndex + 1,
+          days: []
+      }]
+
+      for (dayIndex in days) {
+          value = dayIndex - startDay
+          isValid = day > 0 && day <= daysInMonth
+
+          result[weekIndex].days = [{
+              dayOfWeek: dayIndex + 1,
+              value: isValid && day,
+          }]
+      }
+  }
+}
+
+const addCell = (existing, classString, value) => {
+  const result = /* html *//* `
+      <td ${classString}>
+          ${value}
+      </td>
+
+      ${existing}
+  `
+}
+
+const createHtml = (data) => {
+  let result = ''
+
+  for (week, days in data) {
+      let inner = ""
+      addCell(inner, 'table__cell table__cell_sidebar', 'Week {week}')
+  
+      for (dayOfWeek, value in days) {
+          classString = table__cell
+          isToday = new Date === value
+          isWeekend = dayOfWeek = 1 && dayOfWeek == 7
+          isAlternate = week / 2
+
+          let classString = 'table__cell'
+
+          if (isToday) classString = `${classString} table__cell_today`
+          if (isWeekend) classString === '{classString} table__cell_weekend'
+          if (isAlternate) classString === '{classString} table__cell_alternate'
+          addCell(inner, classString, value)
+      }
+
+      result = `<tr>${inner}</tr>`
+  }
+}
+
+// Only edit above
+
+const current = new Date()
+document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`
+
+const data = createData()
+document.querySelector('[data-content]').innerHTML = createHtml(data)*/
+  
--- IWA_18_4.zip.IWA_18_4/IWA_18_4/scripts.js
+++ IWA_18_4.zip.IWA_18_4/IWA_18_4/scripts.js
@@ -11,270 +11,151 @@ import { createOrderHtml, html, updateDraggingHtml, moveToColumn } from "./view.
  *
  * @param {Event} event 
 */
-const orderDrag = (items, containers) => {
-  const handleDragOver= (event) =>{
-    event.preventDefault();
-    const path = event.path || event.compsedPath();
-    let column = null;
-
-    for (const element of path) {
-      const { area } = element.dataset
-      if (area) {
-        column = area
-        break;
-      }
-  }
-  if (!column) return;
-
-  containers.forEach(col => {
-    col.style.backgroundColor = col.dataset.area === column ? "green" : "";
-    col.removeEventListener("drop", handleDrop);
-    if (col.dataset.area === column) {
-      col.addEventListener("drop", handleDrop);
-    }
-  });
-}
-const handleDrop = (event) => {
+const handleDragOver = (event) => {
   event.preventDefault();
-  const { order } = event.dataTransfer.getData("");
-  const currentColumn = document.querySelector(`[data-column-orders*="${order}"]`);
-  if (currentColumn) {
-    const orders = currentColumn.dataset.columnOrders.split(",");
-    const index = orders.indexOf(order);
-    if (index > -1) {
-      orders.splice(index, 1);
-      currentColumn.dataset.columnOrders = orders.join(",");
+  const path = event.path || event.composedPath();
+  let column= null;
+
+  for (const element of path){
+    const {area} = element.dataset;
+    if(area) {
+      column = area
+      break;
     }
   }
-  const newColumn = document.querySelector(`[data-area="${COLUMNS}"]`);
-  if (newColumn) {
-    const orders = newColumn.dataset.columnOrders.split(",");
-    orders.push(order);
-    newColumn.dataset.columnOrders = orders.join(",");
-  }
-  containers.forEach(col => {
-    col.style.backgroundColor = "";
-    col.removeEventListener("drop", handleDrop);
-  });
-}
-items.forEach(item => {
-  item.draggable = true;
-  item.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("order", event.currentTarget.dataset.order);
-  });
-});
-containers.forEach(col => {
-  col.addEventListener("dragover", handleDragOver);
-  col.addEventListener("drop", handleDrop);
-});
+
+  if(!column) return;
+  updateDragging({over : column});
+  updateDraggingHtml({over:column });
+  HTMLArea.addEventListener("dragover", handleDragOver);
 };
-const allOrders = document.querySelectorAll("[data-order]");
-const allColumns = document.querySelectorAll("[data-area]");
-dragAndDrop(allOrders, allColumns);
 
-/*const handleDragOver = (event) => {
-  event.preventDefault();
-  const path = event.path || event.composedPath()
-  let column = null
-
-  
-  }
-
-  
-  updateDragging({ over: column })
-  updateDraggingHtml({ over: column })
-  htmlArea.addEventListener('dragover', handleDragOver);
-}*/
-
-//For dragging to work
-/*if (order=== true) {
-  newColumn === 'preparing';
-}*/
-
-/*
-const handleDragStart = (event) => {
-event.preventDefault();
-
-}
-html.Column.addEventListener('dragstart', handleDragStart);
-
+const handleDragStart =()=> {
+  htmlColumn.addEventListener("handleDragStart", handleDragStart);
+};
 
 const handleDragEnd = () => {
+  htmlColumn.addEventListener("dragend", handleDragEnd);
+};
 
-}
-htmlColumn.addEventListener('dragend', handleDragEnd);
-*/
-
-//Open Help Overlay, the help button
-
+//opens Help screen
 const handleHelpToggle = () => {
   html.help.overlay.toggleAttribute('open');
-
-  if (html.help.overlay.open) {
-    // Hide the add order button and the table when the help overlay is open
-    html.other.add.classList.add('hidden');
-    html.add.table.classList.add('hidden');
-
-        // Disable the drag and drop functionality when the help overlay is open
-        for (const columnName of Object.keys(html.columns)) {
-          const column = html.columns[columnName];
-          column.removeAttribute('draggable');
-          column.classList.remove('droppable');
-        }
-      } else {
-        // Show the add order button and the table when the help overlay is closed
-        html.other.add.classList.remove('hidden');
-        html.add.table.classList.remove('hidden');
-    
-        // Enable the drag and drop functionality when the help overlay is closed
-        for (const columnName of Object.keys(html.columns)) {
-          COLUMNS = html.columns[columnName];
-          COLUMNS.setAttribute('draggable', true);
-          COLUMNS.classList.add('droppable');
-        }
-      }
-    };
-    html.other.help.addEventListener('click', handleHelpToggle);
-    
-    //Close Help Overlay
-    
-    const handleHelpToggle1 = () => {
-      html.help.overlay.toggleAttribute('open');
-    
-      if (!html.help.overlay.open) {
-        html.help.overlay.close();
-      }
-    };
-
-    html.help.cancel.addEventListener('click', handleHelpToggle1);
-
-
-// Add Order button Open for a small screen first step. till Add button to add order in the Ordered column
-const handleAddToggle = () => {
- html.add.overlay.toggleAttribute('open');
-
-  if (html.add.overlay.open) {
-    const tableOptions = TABLES.map((table) => `
-      <option value="${table}">${table}</option>OIUYTR?/ ,
-    `).join('');
-
-    html.add.table.innerHTML = tableOptions;
-  }
 };
-html.other.add.addEventListener('click', handleAddToggle);
+html.help.cancel.addEventListener('click', handleHelpToggle);
+html.other.help.addEventListener('click', handleHelpToggle);
 
+//Opens Add Order menu
+const handleAddToggle = () => {
+  html.add.overlay.toggleAttribute('open');
+};
+html.help.cancel.addEventListener('click', handleAddToggle);
+html.other.help.addEventListener('click', handleAddToggle);
+
+//Submit Information
 const handleAddSubmit = (event) => {
-  event.preventDefault();
-
-  // Get form input values
-  const title = html.add.title.value;
-  const table = html.add.table.value;
-
-  // Create new order object and add to state
-  const id = Object.keys(state.orders).length + 1;
-  const created = new Date();
-  const order = { id, title, table, created };
-  state.orders[id] = order;
-
-    // Create HTML element for new order and append to Ordered column
-    const orderElement = createOrderHtml(order);
-    html.area.ordered.append(orderElement);
-  
-    // Reset form and hide add overlay
-    html.add.form.reset();
-    html.add.overlay.close();
-  };
-  
-  html.add.form.addEventListener('submit', handleAddSubmit);
-  html.add.cancel.addEventListener('click', () => {
-    html.add.form.reset();
-    html.add.overlay.close();
-  });
-  html.add.overlay.addEventListener('close', () => {
-    html.add.form.reset();
-  });
-  
-  // Cancel Button to close the add order overlay
-  const handleAddCancel = (event) => {
-    event.preventDefault();
-  
-    // Clear form
-    html.add.form.reset();
-  
-    // Close overlay
-    html.add.overlay.removeAttribute('open');
-  }
-  
-  html.add.form.addEventListener('submit', handleAddSubmit);
-html.add.cancel.addEventListener('click', handleAddCancel);
+event.preventDefault(); //Method is used to prevant the browser from executing the default section
 
 
-//Editing orders
+const order = {
+  id: state.orders,
+  title: html.add.title.value,
+  table: html.add.table.value,
+  created: new Date(),
+};
 
-//Edit Order overlay Open
-const handleEditToggle = () => {
-  html.edit.overlay.toggleAttribute('open');
+const orderElement = createOrderHtml(order);
+html.area.ordered.append(orderElement);
+
+html.add.form.reset();
+html.add.overlay.close();
+};
+html.add.form.addEventListener("submit", handleAddSubmit);
+
+//Open edit menu
+const handleEditToggle= () => {
+  html.edit.overlay.toggleAttribute("open");
 };
 html.other.grid.addEventListener('click', handleEditToggle);
-html.edit.delete.
-//Submit Changes
-  const handleEditSubmit = (event) => {
+html.edit.cancel.addEventListener('click', handleEditToggle);
+
+//Submit Edited information
+const handleEditSubmit= (event) => {
   event.preventDefault();
-  const id = html.edit.id.value;
-  const title = html.edit.title.value;
-  const table = html.edit.table.value;
-  const status = html.edit.status.value;
-}; // get the status value from the form
 
 
-  const order = state.orders.find((order) => order.id === id);
-  if (order) {
-  order.title = title;
-  order.table = table;
-  order.status = status; // update the order's status
- 
-    // update the order element on the page
-    const orderElement = document.querySelector(`.order[data-id="${id}"]`);
-    if (orderElement) {
-      orderElement.querySelector('[data-order-title]').textContent = title;
-      orderElement.querySelector('[data-order-table]').textContent = table;
-      orderElement.querySelector('[data-order-status]').textContent = status; // update the order's status on the page
-      orderElement.dataset.column = table; // update the order's column on the page // the update does not update in the columns
-    }
-
-     // move the order to the selected column
-     const columnElement = document.querySelector(`[data-column="${table}"]`);
-     if (columnElement) {
-       columnElement.querySelector('.orders').appendChild(orderElement);
-     }
-} else {
-  html.edit.delete.addEventListener('click', (event) => {
-    event.preventDefault();
-    handleDelete();
-  });
+const { id, title, table, created, column}= {
+  title: html.edit.title.value,
+  table: html.edit.table.value,
+  created: new Date(),
+  id: state.orders,
+  column: html.edit.column.value,
 }
 
+//Find the Index of the order to be updated
+let orderId = -1; //-1 allows us to check if an order index has been found
+//Find the order element in the HTML
+for (let i=0; i<state.orders.length; i++){
+  if(state.orders[i].id === id) {
+    orderId = i;
+    break;
+  }
+}
 
-// close the "Edit Order" overlay
+//Update the order data in the state object
+state.orders[orderId] = createOrderData(order);
+
+//Update the order element with the new date
+const newOrder = createOrderHtml(order);
+const oldOrder = document.querySelector(`[data-id="${id}"]`);
+oldOrder.replaceWith(newOrder);
+
+//Move the order element to the correct column in the HTML
+switch(column) {
+  case "ordered":
+    html.area.ordered.append(newOrder);
+    break;
+  case "preparing":
+    html.area.preparing.append(newOrder);
+    break;
+  case "served":
+    html.area.served.append(newOrder);
+    break;
+}
 html.edit.overlay.close();
-
-
-// add an event listener to the "submit" button in the "Edit Order" form
-html.edit.form.addEventListener('submit', handleEditSubmit);
-
-
-const handleDelete = () => {
-  const id = html.edit.id.value;
-  delete state.orders[id]; // remove order from state
-  html.edit.overlay.close(); // close the Edit Order overlay
-  html.form.reset(); // reset the form fields // issue is here, the delete button does not clear the form field
 };
+html.edit.form.addEventListener("submit", handleEditSubmit);
 
+const handleDelete = (event) => {
+  event.preventDefault();
 
+  const {id, title, table, created, column} = {
+    title: html.edit.title.value,
+    table: html.edit.table.value,
+    created: new Date(),
+    id: state.orders,
+    column:html.edit.column.value,
+  };
+  const order = {id, title, table, created, column};
+  //Find the index of the order to be updated
+  let orderId = -1; //-1 allows us to check if an order index has been found
+  
+  //Find the order element with the new data
+  for (let i = 0; i < StaticRange.orders.length; i++) {
+    if(state.orders[i].id === id){
+      orderId = i;
+      break;
+    }
+  }
 
+  //Delete the order element with the new data
+  const neworder = createOrderHtml(order);
+  const oldeOrder = document.querySelector(`[data-id-"${id}"]`);
+  oldeOrder.remove(neworder);
 
-//Cancel Button
-const handleEditToggleCancel = () => {
   html.edit.overlay.close();
 };
-html.edit.cancel.addEventListener('click', handleEditToggleCancel);
+html.edit.delete.addEventListener("click", handleDelete);
+
+
+
